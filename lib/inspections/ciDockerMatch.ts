cc7189e6ede8c2b3200c172f8cd610555bfa0138
ipcrm <mcadorette@gmail.com>
2018-12-22 17:48:14 -0500

    finialize ci config fix example

--- lib/inspections/ciDockerMatch.ts
+++ lib/inspections/ciDockerMatch.ts
@@ -5,6 +5,10 @@ import {
   slackInfoMessage,
   ReviewListenerRegistration,
   CodeInspectionRegistration,
+  actionableButton,
+  slackFooter,
+  CodeTransform,
+  CodeTransformRegistration,
 } from "@atomist/sdm";
 import {
   NoParameters,
@@ -12,11 +16,14 @@ import {
   logger,
   ProjectReview,
   ReviewComment,
+  Project,
 } from "@atomist/automation-client";
 import { DockerFileParser } from "@atomist/sdm-pack-docker";
 import * as yaml from "js-yaml";
 import { diff } from "deep-diff";
 import _ = require("lodash");
+import { SlackMessage } from "@atomist/slack-messages";
+import { PullRequest } from "@atomist/automation-client/lib/operations/edit/editModes";
 
 export interface CiDockerMatchResult {
     result: boolean;
@@ -24,16 +31,33 @@ export interface CiDockerMatchResult {
     repoId: string;
 }
 
-export const ciDockerMatch: CodeInspection<ProjectReview, NoParameters> = async p => {
-    const imageDetails = [];
-    if (await p.hasFile("Dockerfile") && await p.hasFile(".drone.yml")) {
+export interface DockerImageVersion {
+    image: string;
+    version: string;
+}
+
+export const getDockerfileImageVersion = (p: Project) => {
+    return new Promise<DockerImageVersion>( async (res, rej) => {
         try {
             // Parse Dockerfile
             const imageName: string[] = await astUtils.findValues(
                 p, DockerFileParser, "Dockerfile", "//FROM/image/name");
             const imageVersion: string[] = await astUtils.findValues(
                 p, DockerFileParser, "Dockerfile", "//FROM/image/tag");
-            imageDetails.push({image: imageName[0], version: imageVersion[0]});
+
+            res({image: imageName[0], version: imageVersion[0]});
+        } catch (e) {
+            logger.debug(`Error! ${e}`);
+            rej(e);
+        }
+    });
+};
+
+export const ciDockerMatch: CodeInspection<ProjectReview, NoParameters> = async p => {
+    const imageDetails = [];
+    if (await p.hasFile("Dockerfile") && await p.hasFile(".drone.yml")) {
+        try {
+            imageDetails.push(await getDockerfileImageVersion(p));
 
             // Parse Drone file
             const droneFileHandle = await p.getFile(".drone.yml");
@@ -58,8 +82,8 @@ export const ciDockerMatch: CodeInspection<ProjectReview, NoParameters> = async 
                     newDiffs.forEach(nD => {
                         const dockerFileValue = _.get(nD, "lhs");
                         const droneFileValue = _.get(nD, "rhs");
-                        message.push(`\n${dP.charAt(0).toUpperCase() + dP.slice(1)} value doesn't match!` +
-                        ` Dockerfile value: ${dockerFileValue} vs Drone config: ${droneFileValue}`);
+                        message.push(`\n*${dP.charAt(0).toUpperCase() + dP.slice(1)} value doesn't match!*` +
+                        `\n\tDockerfile value: *${dockerFileValue}*\n\tDrone config: *${droneFileValue}*`);
                     });
                 });
 
@@ -90,13 +114,43 @@ export const ciDockerMatch: CodeInspection<ProjectReview, NoParameters> = async 
 
 export const optionalFailGoalsIfCiConfigDoesntMatch: ReviewListener = async rli => {
     if (rli.review.comments && rli.review.comments. length > 0) {
-        rli.review.comments.forEach(c => {
-            rli.addressChannels(slackInfoMessage(
-                `Drone CI and Dockerfile Configuration Mismatch`,
-                c.detail,
+        rli.review.comments.forEach(async c => {
+            await rli.addressChannels(slackInfoMessage(
+                `Error: Drone CI and Dockerfile Configuration Mismatch!`,
+                "\n" + c.detail,
             ));
         });
-        return PushImpactResponse.requireApprovalToProceed;
+
+        const updateMsg: SlackMessage = {
+            attachments: [
+                {
+                    text: `Automatically correct this error?`,
+                    fallback: "none",
+                    actions: [
+                        actionableButton(
+                            {
+                                text: "Correct CI Configuration Mismatch",
+                            },
+                            correctCiConfiguration,
+                            {
+                                targets: {
+                                    owner: rli.id.owner,
+                                    repo: rli.id.repo,
+                                    branch: rli.id.branch,
+                                    url: rli.id.url,
+                                    sha: rli.id.sha,
+                                },
+                            },
+                        ),
+                    ],
+                    color: "#ffcc00",
+                    footer: slackFooter(),
+                },
+            ],
+        };
+
+        await rli.addressChannels(updateMsg);
+        return PushImpactResponse.failGoals;
     }
     return PushImpactResponse.proceed;
 };
@@ -105,9 +159,41 @@ export const ciDockerMatchRegistration: CodeInspectionRegistration<ProjectReview
     name: "CI Docker Match Test",
     description: "Test if CI configuration is correct; does Dockerfile and Drone config use the same image?",
     inspection: ciDockerMatch,
-}
+};
 
 export const optionalFailGoalsIfCiConfigDoesntMatchRegistration: ReviewListenerRegistration = {
     name: "Optional fail goals if CI configuration doesn't match",
     listener: optionalFailGoalsIfCiConfigDoesntMatch,
+};
+
+export const makeDroneConfigMatchDockerfileImageVersion: CodeTransform<NoParameters> = async p => {
+    if (await p.hasFile("Dockerfile") && await p.hasFile(".drone.yml")) {
+        // Get Dockerfile details
+        const dockerDetails = await getDockerfileImageVersion(p);
+
+        // Parse Drone file
+        const droneFileHandle = await p.getFile(".drone.yml");
+        const droneFileYaml = yaml.safeLoad(await droneFileHandle.getContent());
+        droneFileYaml.pipeline["install-and-build"].image = `${dockerDetails.image}:${dockerDetails.version}`;
+
+        // Write new Drone file
+        await droneFileHandle.setContent(yaml.dump(droneFileYaml));
+        return p;
+    }
+    return p;
+};
+
+const AtomistGeneratedMarker = "[atomist:generated]";
+const correctCiMarker = "[atomist:fix-drone-ci-configuration]";
+
+export const correctCiConfiguration: CodeTransformRegistration = {
+    transform: makeDroneConfigMatchDockerfileImageVersion,
+    name: "correctCiConfiguration",
+    transformPresentation: () => new PullRequest(
+        `correct-ci-config-${Date.now()}`,
+        "Correct Drone CI Configuration",
+        `Update Drone CI configuration so that the image used matches the Dockerfile
+    ${AtomistGeneratedMarker}`,
+        `Fix Drone CI Configuration
+${correctCiMarker}`),
 };
--- lib/machine/machine.ts
+++ lib/machine/machine.ts
@@ -14,25 +14,20 @@
  * limitations under the License.
  */
 
-import { GitHubRepoRef, editModes } from "@atomist/automation-client";
 import {
-    allSatisfied,
     AutoCodeInspection,
     Autofix,
-    Fingerprint,
     goalContributors,
     goals,
-    LogSuppressor,
-    not,
     onAnyPush,
     PushImpact,
     SoftwareDeliveryMachine,
     SoftwareDeliveryMachineConfiguration,
-    ToDefaultBranch,
     whenPushSatisfies,
-    GoalWithFulfillment,
-    Goals,
 } from "@atomist/sdm";
+import {
+    Build,
+} from "@atomist/sdm-pack-build";
 import {
     createSoftwareDeliveryMachine,
     DisableDeploy,
@@ -43,71 +38,9 @@ import {
     Version,
 } from "@atomist/sdm-core";
 import {
-    Artifact,
-    Build,
-} from "@atomist/sdm-pack-build";
-import {
-    CloudFoundryDeploy,
-    CloudFoundryDeploymentStrategy,
-    CloudFoundrySupport,
-    HasCloudFoundryManifest,
-} from "@atomist/sdm-pack-cloudfoundry";
-import {
-    DockerBuild,
-    HasDockerfile,
-} from "@atomist/sdm-pack-docker";
-import {
-    fingerprintSupport,
-    logbackFingerprints,
-    applyFingerprint,
-    fingerprintImpactHandler,
-    messageMaker,
-} from "@atomist/sdm-pack-fingerprints";
-import {
-    createNpmDepsFingerprints,
-    applyNpmDepsFingerprint,
-} from "@atomist/sdm-pack-fingerprints/lib/fingerprints/npmDeps";
-import {
-    applyDockerBaseFingerprint,
-    dockerBaseFingerprint,
-} from "@atomist/sdm-pack-fingerprints/lib/fingerprints/dockerFrom";
-import {
-    KubernetesDeploy,
-    kubernetesSupport,
-} from "@atomist/sdm-pack-k8";
-import {
-    IsNode,
-    nodeBuilder,
-    NodeProjectVersioner,
-    NpmProgressReporter,
-    NpmVersionProjectListener,
-    NodeProjectCreationParametersDefinition,
-    UpdatePackageJsonIdentification,
-    UpdateReadmeTitle,
-    NpmCompileProjectListener,
-} from "@atomist/sdm-pack-node";
-import {
-    IsMaven,
-    mavenBuilder,
-    MavenDefaultOptions,
-    MavenProjectVersioner,
-    MvnPackage,
-    MvnVersion,
-    ReplaceReadmeTitle,
-    SetAtomistTeamInApplicationYml,
-    SpringProjectCreationParameterDefinitions,
-    SpringProjectCreationParameters,
-    springSupport,
-    TransformSeedToCustomProject,
+    springSupport, MavenProjectVersioner, MavenDefaultOptions, mavenBuilder, IsMaven,
 } from "@atomist/sdm-pack-spring";
-import { changelogSupport } from "@atomist/sdm-pack-changelog";
-import { IssueSupport } from "@atomist/sdm-pack-issue";
-import {
-    hasJenkinsfile,
-    npmHasBuildScript,
-} from "../support/preChecks";
 import { AddDockerFile } from "../transform/addDockerfile";
-import { AddFinalNameToPom } from "../transform/addFinalName";
 import { AddJenkinsfileRegistration } from "../transform/addJenkinsfile";
 import { AddLicenseFile } from "../transform/addLicense";
 import {
@@ -116,16 +49,12 @@ import {
 } from "../transform/smallMemory";
 import { UpdateDockerfileMaintainer } from "../transform/updateDockerFileMaintainer";
 import { SuggestAddingDockerfile } from "../support/suggestAddDockerfile";
-import { checkNpmCoordinatesImpactHandler } from "@atomist/sdm-pack-fingerprints/lib/machine/FingerprintSupport";
 import { presentSetFingerprints } from "../support/showFingerprints";
 import {
   ciDockerMatchRegistration,
   optionalFailGoalsIfCiConfigDoesntMatchRegistration,
+  correctCiConfiguration,
 } from "../inspections/ciDockerMatch";
-// import { sonarQubeSupport } from "@atomist/sdm-pack-sonarqube";
-// import {
-//     AutoCheckSonarScan,
-// } from "../support/sonarQube";
 
 export function machine(
     configuration: SoftwareDeliveryMachineConfiguration,
@@ -143,39 +72,25 @@ export function machine(
         .addCodeTransformCommand(AddDockerFile)
         .addCodeTransformCommand(AddJenkinsfileRegistration)
         .addCodeTransformCommand(UpdateDockerfileMaintainer)
-        .addCodeTransformCommand(FixSmallMemory);
+        .addCodeTransformCommand(FixSmallMemory)
+        .addCodeTransformCommand(correctCiConfiguration);
 
-    // Fingerprint Compliance
-    const fingerprintComplianceGoal = new GoalWithFulfillment(
-        {
-            uniqueName: "fingerprint-compliance-check",
-            displayName: "fingerprint-compliance-check",
-            definition: {
-                plannedDescription: "Fingerprint Goal Compliance Check",
-                canceledDescription: "Fingerprint Compliance Check Cancelled",
-                failedDescription: "Fingerprint Compliance Check Failed",
-                completedDescription: "Fingerprint Goal Compliance Check Succeeded",
-                workingDescription: "Fingerprint Compliance Check Running...",
-            },
-        },
-    ).with(
-        {
-            name: "backpack-react-waiting",
-        },
-    );
+    // Maven
+    const mavenVersion = new Version().withVersioner(MavenProjectVersioner);
+    const mavenBuild = new Build()
+    .with({
+        ...MavenDefaultOptions,
+        name: "maven-run-build",
+        builder: mavenBuilder([{ name: "maven-run-build" }]),
+        pushTest: MavenDefaultOptions.pushTest,
+    });
 
-    const fingerprint = new Fingerprint();
-    const FingerprintingGoals: Goals = goals("check fingerprints")
-        .plan(fingerprint, fingerprintComplianceGoal);
 
     // Channel Link Listenrers
     sdm.addChannelLinkListener(SuggestAddingDockerfile);
 
     // Global
     const pushImpact = new PushImpact();
-
-    // Artifact
-    const artifact = new Artifact();
 
     // Autofix
     const autofix = new Autofix()
@@ -187,91 +102,8 @@ export function machine(
         .with(ciDockerMatchRegistration)
         .withListener(optionalFailGoalsIfCiConfigDoesntMatchRegistration);
 
-    // Versioners
-    const mavenVersion = new Version().withVersioner(MavenProjectVersioner);
-    const nodeVersion = new Version().withVersioner(NodeProjectVersioner);
-
-    // Builds
-    const mavenBuild = new Build()
-        .with({
-            ...MavenDefaultOptions,
-            name: "maven-run-build",
-            builder: mavenBuilder([{ name: "maven-run-build" }]),
-            pushTest: MavenDefaultOptions.pushTest,
-        });
-
-    const externalBuild = new Build()
-        .with({
-            externalTool: "jenkins",
-            pushTest: hasJenkinsfile,
-        });
-
-    const nodeBuild = new Build()
-        .with({
-            logInterpreter: LogSuppressor,
-            progressReporter: NpmProgressReporter,
-            name: "node-run-build",
-            builder: nodeBuilder("npm install", "npm run build"),
-            pushTest: IsNode,
-        });
-
-    const dockerBuild = new DockerBuild()
-        .with({
-            options: { push: true, ...sdm.configuration.sdm.dockerinfo },
-            pushTest: allSatisfied(IsMaven, HasDockerfile),
-        })
-        .withProjectListener(MvnVersion)
-        .withProjectListener(MvnPackage)
-        .withProjectListener(NpmCompileProjectListener)
-
-        .with({
-            options: { push: true, ...sdm.configuration.sdm.dockerinfo },
-            pushTest: allSatisfied(IsNode, HasDockerfile),
-        })
-        .withProjectListener(NpmVersionProjectListener);
-
-    // Kubernetes Deploys
-    const k8sStagingDeploy = new KubernetesDeploy({ environment: "testing", approval: true });
-    const k8sProductionDeploy = new KubernetesDeploy({ environment: "production" });
-
-    const k8sDeployGoals = goals("deploy")
-        .plan(k8sStagingDeploy).after(dockerBuild)
-        .plan(k8sProductionDeploy).after(k8sStagingDeploy);
-
-    // CF Deployment
-    const cfDeployment = new CloudFoundryDeploy({
-        displayName: "Deploy to CF `production`",
-        environment: "production",
-        preApproval: true,
-        descriptions: {
-            inProcess: "Deploying to Cloud Foundry `production`",
-            completed: "Deployed to Cloud Foundry `production`",
-        },
-    })
-        .with({ environment: "production", strategy: CloudFoundryDeploymentStrategy.API });
-
-    const cfDeploymentStaging = new CloudFoundryDeploy({
-            displayName: "Deploy to CF `testing`",
-            environment: "testing",
-            preApproval: true,
-            descriptions: {
-                inProcess: "Deploying to Cloud Foundry `testing`",
-                completed: "Deployed to Cloud Foundry `testing`",
-            },
-        },
-    )
-        .with({ environment: "staging", strategy: CloudFoundryDeploymentStrategy.API });
-
-    const pcfDeploymentGoals = goals("cfdeploy")
-        .plan(cfDeploymentStaging).after(mavenBuild)
-        .plan(cfDeployment).after(cfDeploymentStaging);
-
     // Ext Packs setup
     sdm.addExtensionPacks(
-        // sonarQubeSupport({
-        //     ...sdm.configuration.sdm.sonar,
-        //     inspectGoal: codeInspection,
-        // }),
         springSupport({
             review: {
                 springStyle: true,
@@ -284,135 +116,25 @@ export function machine(
             autofixGoal: autofix,
             reviewListeners: [],
         }),
-        kubernetesSupport(),
-        CloudFoundrySupport({
-            pushImpactGoal: pushImpact,
-        }),
         gitHubGoalStatus(),
         goalState(),
-        changelogSupport(),
-        IssueSupport,
-        fingerprintSupport(
-            fingerprint,
-            [
-                {
-                    extract: p => logbackFingerprints(p.baseDir),
-                    apply: (p, fp) => applyFingerprint(p.baseDir, fp),
-                    selector: fp => fp.name === "elk-logback",
-                },
-                {
-                    extract: createNpmDepsFingerprints,
-                    apply: applyNpmDepsFingerprint,
-                    selector: fp => fp.name.startsWith("npm-project-dep"),
-                },
-                {
-                    apply: applyDockerBaseFingerprint,
-                    extract: dockerBaseFingerprint,
-                    selector: myFp => myFp.name.startsWith("docker-base-image"),
-                },
-            ],
-            checkNpmCoordinatesImpactHandler(),
-            fingerprintImpactHandler(
-                {
-                    complianceGoal: fingerprintComplianceGoal,
-                    transformPresentation: ci => {
-                        return new editModes.PullRequest(
-                            `apply-target-fingerprint-${Date.now()}`,
-                            `Apply fingerprint ${ci.parameters.fingerprint} to project`,
-                            "Nudge generated by Atomist");
-                    },
-                    messageMaker,
-                },
-            ),
-        ),
     );
-
-    // Generators
-    sdm
-        .addGeneratorCommand<SpringProjectCreationParameters>({
-            name: "create-spring",
-            intent: "create spring",
-            description: "Create a new Java Spring Boot REST service",
-            parameters: SpringProjectCreationParameterDefinitions,
-            startingPoint: GitHubRepoRef.from({ owner: "atomist-seeds", repo: "spring-rest", branch: "master" }),
-            transform: [
-                ReplaceReadmeTitle,
-                SetAtomistTeamInApplicationYml,
-                TransformSeedToCustomProject,
-                AddFinalNameToPom,
-            ],
-        })
-        .addGeneratorCommand<SpringProjectCreationParameters>({
-            name: "create-spring-external-build",
-            intent: "create spring jenkins build",
-            description: "Create a new Java Spring Boot REST service that builds with Jenkins",
-            parameters: SpringProjectCreationParameterDefinitions,
-            startingPoint: GitHubRepoRef.from({ owner: "ipcrmdemo", repo: "spring-rest-jenkins", branch: "master" }),
-            transform: [
-                ReplaceReadmeTitle,
-                SetAtomistTeamInApplicationYml,
-                TransformSeedToCustomProject,
-                AddFinalNameToPom,
-            ],
-        })
-        .addGeneratorCommand({
-            name: "typescript-express-generator",
-            parameters: NodeProjectCreationParametersDefinition,
-            startingPoint: new GitHubRepoRef("ipcrmdemo", "typescript-node-api"),
-            intent: "create node",
-            transform: [
-                UpdatePackageJsonIdentification,
-                UpdateReadmeTitle,
-            ],
-        });
 
     // global
     const GlobalGoals = goals("global")
-        .plan(autofix, FingerprintingGoals, codeInspection, pushImpact);
+        .plan(autofix, codeInspection, pushImpact);
 
-    // Maven
     const MavenBaseGoals = goals("maven-base")
         .plan(mavenVersion, mavenBuild).after(GlobalGoals);
-
-    // Node
-    const NodeBaseGoals = goals("node-base")
-        .plan(nodeVersion, nodeBuild).after(GlobalGoals);
 
     // Rules
     sdm.addGoalContributions(goalContributors(
         onAnyPush()
             .setGoals(GlobalGoals),
 
-        whenPushSatisfies(IsMaven, not(hasJenkinsfile))
+        whenPushSatisfies(IsMaven)
             .setGoals(MavenBaseGoals),
-
-        whenPushSatisfies(IsNode, npmHasBuildScript, not(hasJenkinsfile))
-            .setGoals(NodeBaseGoals),
-
-        whenPushSatisfies(IsMaven, hasJenkinsfile)
-            .setGoals(goals("maven-external").plan(mavenVersion, externalBuild).after(GlobalGoals)),
-
-        whenPushSatisfies(HasDockerfile)
-            .setGoals(
-                goals("docker-build")
-                    .plan(dockerBuild).after(mavenBuild, nodeBuild, externalBuild)
-                    .plan(artifact).after(dockerBuild),
-            ),
-
-        whenPushSatisfies(HasCloudFoundryManifest, ToDefaultBranch)
-            .setGoals(pcfDeploymentGoals),
-
-        whenPushSatisfies(HasDockerfile, ToDefaultBranch)
-            .setGoals(k8sDeployGoals)));
+    ));
 
     return sdm;
 }
-
-    // sdm.addCommand<{ name: string }>({
-    //     name: "hello",
-    //     intent: "hello",
-    //     parameters: {
-    //         name: { description: "Your name" },
-    //     },
-    //     listener: async cli => cli.addressChannels(`Hello ${cli.parameters.name}`),
-    // });
